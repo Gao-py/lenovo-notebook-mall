94a54cf34fe594da1354b382f4a10f6248014647
yu <19554889358@163.com>
2025-11-26 15:55:50 +0800

    Initial commit

---
+++ src/main/java/org/example/lenovonotebookmall/LenovoNotebookMallApplication.java
@@ -0,0 +1,13 @@
+package org.example.lenovonotebookmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LenovoNotebookMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LenovoNotebookMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/lenovonotebookmall/LenovoNotebookMallApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lenovonotebookmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LenovoNotebookMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
